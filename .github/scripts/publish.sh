e203572b1e744e1b44728cdd411995fafd77d260
Manuthor <manu.coste@gmail.com>
2024-11-25 22:44:49 +0100

    fix: get more distrib

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -3,6 +3,7 @@ set -ex
 
 env
 
-DESTINATION_DIR=/mnt/package/openssl/${OPENSSL_VERSION}/${SUFFIX_DIR}
+DESTINATION_DIR=/mnt/package/openssl/${OPENSSL_VERSION}/${ARCHIVE_NAME}/$(uname -m)
+
 ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR"
 scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa "${OPENSSL_VERSION}.tar.gz" cosmian@package.cosmian.com:"$DESTINATION_DIR"/
